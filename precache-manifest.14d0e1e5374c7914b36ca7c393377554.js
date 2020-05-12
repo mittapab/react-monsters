265d31cb363912999bd62d889423f9475d5e914d
nuttaphon <chanpan.nuttaphon@gmail.com>
2020-05-12 13:35:16 +0700

    Updates

---
+++ precache-manifest.14d0e1e5374c7914b36ca7c393377554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93340a09414a97e47279891d9095b65c",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "6dc45bed3bf3edf54352",
+    "url": "/react-monsters/static/css/main.43cd30a9.chunk.css"
+  },
+  {
+    "revision": "dd4f7504ebb7300fc2b8",
+    "url": "/react-monsters/static/js/2.60c6623f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.60c6623f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc45bed3bf3edf54352",
+    "url": "/react-monsters/static/js/main.c9b1fe1b.chunk.js"
+  },
+  {
+    "revision": "3a70c63b26b1105c6144",
+    "url": "/react-monsters/static/js/runtime-main.495c929d.js"
+  }
+]);
